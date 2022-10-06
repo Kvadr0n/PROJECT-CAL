b447d2e381031743f19ae63ddb081c01495c6d77
sedavSdk <sedavj1@gmail.com>
2022-10-06 04:24:43 +0300

    derive

--- cal/num.cpp
+++ cal/num.cpp
@@ -1,8 +1,44 @@
 #include "num.h"
 
+
 double num::derive(double x)
 {
-    return 0.0;
+    double h = 1;
+    double a = gen::order(x) / 2 - 26;
+    a = a < -1023 ? -1023 : a;
+    gen::exput(h, a);
+    try {
+        return gen::x2((2 * calc::sin(x + h) - 2 * calc::sin(x - h)), -a - 1);
+    }
+    catch (OutOfDomain()) {
+        return gen::x2((2 * calc::sin(x + h) - 2 * calc::sin(x)), -a);
+    }
+}
+double num::derive2(double x)
+{
+    double h = 1;
+    double a = gen::order(x) / 2 - 26;
+    a = a < -1023 ? -1023 : a;
+    gen::exput(h, a);
+    try {
+        return gen::x2((calc::sin(x + h) + calc::ln(x + h) - calc::sin(x - h) - calc::ln(x - h)), -a - 1);
+    }
+    catch (OutOfDomain()) {
+        return gen::x2((calc::sin(x + h) + calc::ln(x + h) - calc::sin(x) - calc::ln(x)), -a);
+    }
+}
+double num::derive3(double x)
+{
+    double h = 1;
+    double a = gen::order(x) / 2 - 26;
+    a = a < -1023 ? -1023 : a;
+    gen::exput(h, a);
+    try {
+        return gen::x2((calc::exp(x + h) - calc::exp(x - h)), -a - 1);
+    }
+    catch (OutOfDomain()) {
+        return gen::x2((calc::exp(x + h) - calc::exp(x)), -a);
+    }
 }
 
 double num::derive(double x, unsigned char n)
@@ -23,6 +59,7 @@ double num::integrate(double a, double b)
 void num::test()
 {
     using namespace num;
+    std::cout << derive3(2.6);
     //Тесты писать сюда
     //Обратить внимание на макросы test1to1errfunc и test2to1errfunc в testmacros.h
 }
